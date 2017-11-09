af0950fbbbbbc1635bf8c941e1a3da46edae8a51
samezi <d_viper@hotmail.co.jp>
2017-11-09 19:46:18 +0900

    アニメーションするスロットのアニメーションを追加。また専用のAnimatorとprefabを作成

---
+++ Assets/script/slotSpinController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotSpinController : MonoBehaviour {
+    public Animator slotAnimator;
+
+	// Use this for initialization
+	void Start () {
+        setSlotNumber(1);
+        slotCountUp();
+        slotCountUp();
+        slotCountUp();
+        slotCountUp();
+        slotCountUp();
+    }
+
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void setSlotNumber(int setNumber)
+    {
+        slotAnimator.SetInteger("InitializeDisplayNumber", setNumber);
+    }
+
+    public void slotCountUp()
+    {
+        slotAnimator.SetTrigger("CountUp");
+    }
+}
