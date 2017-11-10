df34f18ddb87b35619d775e954b79812d0fa2f58
samezi <d_viper@hotmail.co.jp>
2017-11-10 20:17:15 +0900

    マニ車の回転音に関するメソッドを追加

---
+++ Assets/script/wheelVolumeEnhanser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class wheelVolumeEnhanser : MonoBehaviour {
+    public AudioSource audiosource;
+    public AudioMixer mix;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mix.SetFloat("wheelVolume", 0);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void wheelVolumeMaximum()
+    {
+        mix.SetFloat("wheelVolume", 0);
+    }
+
+    public void wheelVolumeMin()
+    {
+        mix.SetFloat("wheelVolume", -80);
+    }
+}
