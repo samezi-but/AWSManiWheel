fc483c6264dbcdbe8accce9864a16e2d48760bc2
samezi <d_viper@hotmail.co.jp>
2017-11-11 08:52:07 +0900

    自分の功徳のセーブ/ロード機能のメソッドを作成

---
+++ Assets/yourKudokuSaveAndLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yourKudokuSaveAndLoad : MonoBehaviour {
+    private const string yourKudoku = "KUDOKU";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void saveKudoku(ulong kudokuCount)
+    {
+        string ketaStringCount = kudokuCount.ToString();
+        PlayerPrefs.SetString(yourKudoku, ketaStringCount);
+    }
+
+    public ulong loadKudoku()
+    {
+        string tempString = PlayerPrefs.GetString(yourKudoku, "0");
+        ulong returnUlong = ulong.Parse(tempString);
+
+        return returnUlong;
+    }
+}
