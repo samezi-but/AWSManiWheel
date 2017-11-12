924a3a132cd7ca44d8823a27e3e5bf02952b2e51
samezi <d_viper@hotmail.co.jp>
2017-11-12 23:24:08 +0900

    カウンターが繰り上がらないのを直したつもり

---
+++ Assets/editor/saveEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(yourKudokuSaveAndLoad))]
+public class saveEditor : Editor
+{
+    // 調査用メソッド
+    private string setCounter;
+
+    public override void OnInspectorGUI()
+    {
+
+        //　シリアライズオブジェクトの更新
+        serializedObject.Update();
+        Rect rect = GUILayoutUtility.GetRect(10, 20);
+        if (GUILayout.Button("ローカルにセーブ"))
+        {
+            PlayerPrefs.SetString("KUDOKU", "999998");
+        }
+        //　シリアライズオブジェクトのプロパティの変更を更新
+        serializedObject.ApplyModifiedProperties();
+
+
+    }
+}
